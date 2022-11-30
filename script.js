a8b2fd98b8b6ff1609af03ee0c1c4c711fb823e0
Joana Areas <jo_areas@hotmail.com>
2022-11-30 20:25:17 -0300

    refactoring with ternary operator

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let highScore = 0;
 //playing game
 document.querySelector('.check').addEventListener('click',function () {
   const guess = Number(document.querySelector('.guess').value) //any input from the user always comes as a string, so we have to convert it into a number
-  console.log(guess);
-  if (!guess) { //we have to check if the input is empty. If it is, it will be 0, which is a falsy value
+  //we have to check if the input is empty. If it is, it will be 0, which is a falsy value
+  if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!'
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '👏 Correct number!'
@@ -20,20 +20,9 @@ document.querySelector('.check').addEventListener('click',function () {
       highScore = score
       document.querySelector('.highscore').textContent = highScore;
     }
-
-  } else if (guess > secretNumber) {
+  } else if(guess !== secretNumber){
     if (score > 1 ) {
-      document.querySelector('.message').textContent = 'Too high!'
-      score--;
-      scoreElement.textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😕 You lost the game!'
-      scoreElement.textContent = 0;
-    }
-
-  } else if (guess < secretNumber) {
-    if (score > 1 ) {
-      document.querySelector('.message').textContent = 'Too low!'
+      document.querySelector('.message').textContent = guess > secretNumber ? 'Too high!' : 'Too low!';
       score--;
       scoreElement.textContent = score;
     } else {
@@ -53,5 +42,4 @@ document.querySelector('.again').addEventListener('click', function(){
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
-
 });
