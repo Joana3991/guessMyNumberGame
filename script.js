43af5e00e5fe013b43203218f4127ee20db41dee
Joana Areas <jo_areas@hotmail.com>
2022-11-30 17:30:33 -0300

    ädded event listener to check buttom

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '👏 Correct number!'
+document.querySelector('.number').textContent = 20;
+document.querySelector('.score').textContent = 13;
+
+document.querySelector('.check').addEventListener('click',function () {console.log(document.querySelector('.guess').value)})
