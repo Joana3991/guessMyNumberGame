69e6b4b5ea7dcf4d52d24b0b1275206b763e57a4
Joana Areas <jo_areas@hotmail.com>
2022-11-30 17:51:21 -0300

    implemented game logic (if else statements)

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '👏 Correct number!'
-document.querySelector('.number').textContent = 20;
-document.querySelector('.score').textContent = 13;
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.score').textContent = 13;
 
-document.querySelector('.check').addEventListener('click',function () {console.log(document.querySelector('.guess').value)})
+const secretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretNumber;
+
+
+document.querySelector('.check').addEventListener('click',function () {
+  const guess = Number(document.querySelector('.guess').value) //any input from the user always comes as a string, so we have to convert it into a number
+  console.log(guess);
+  if (!guess) { //we have to check if the input is empty. If it is, it will be 0, which is a falsy value
+    document.querySelector('.message').textContent = '⛔️ No number!'
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '👏 Correct number!'
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!'
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!'
+  }
+})
